f029848866a6e5a82a81163661bd03c56266a38b
kamala96 <jovinjohn959@gmail.com>
2022-05-31 00:33:02 +0300

    updated view saccos page

--- saccoss_prediction/static/datatables-demo.js
+++ saccoss_prediction/static/datatables-demo.js
@@ -14,3 +14,48 @@ $(document).ready(function () {
     ]
   });
 });
+
+$(document).ready(function () {
+  $('#dataTable1').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      'copy', 'csv', 'excel', 'pdf', 'print'
+    ]
+  });
+});
+
+$(document).ready(function () {
+  $('#dataTable2').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      'copy', 'csv', 'excel', 'pdf', 'print'
+    ]
+  });
+});
+
+$(document).ready(function () {
+  $('#dataTable3').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      'copy', 'csv', 'excel', 'pdf', 'print'
+    ]
+  });
+});
+
+$(document).ready(function () {
+  $('#dataTable4').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      'copy', 'csv', 'excel', 'pdf', 'print'
+    ]
+  });
+});
+
+$(document).ready(function () {
+  $('#dataTable5').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      'copy', 'csv', 'excel', 'pdf', 'print'
+    ]
+  });
+});
