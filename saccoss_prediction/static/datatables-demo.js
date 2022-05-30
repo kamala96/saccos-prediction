5fe95a3908fa7c6eb4dda1e6511766d6f5b788f9
kamala96 <jovinjohn959@gmail.com>
2022-05-31 00:15:16 +0300

    data-table updates

--- saccoss_prediction/static/datatables-demo.js
+++ saccoss_prediction/static/datatables-demo.js
@@ -1,4 +1,16 @@
 // Call the dataTables jQuery plugin
-$(document).ready(function() {
-  $('#dataTable').DataTable();
+
+
+// $(document).ready(function () {
+//   $('#dataTable').DataTable();
+// });
+
+
+$(document).ready(function () {
+  $('#dataTable').DataTable({
+    dom: 'Bfrtip',
+    buttons: [
+      'copy', 'csv', 'excel', 'pdf', 'print'
+    ]
+  });
 });
